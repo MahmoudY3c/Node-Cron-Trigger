63d1b77ceb3b0314718fb28c0f65d18888bc5074
MahmoudY3c <wpsmahmoud25@gmail.com>
2024-03-12 01:47:59 +0200

    project starter files

---
+++ src/NodeCronTrigger.ts
@@ -0,0 +1,49 @@
+import cron, { ScheduleOptions, ScheduledTask } from 'node-cron';
+import { initializeTasks, updateTasksDate } from './jobs-server-start-trigger';
+
+export interface ITask {
+  task: Function;
+  schedule: /* cronStringSyntax */ string;
+  options?: ScheduleOptions;
+}
+
+export interface ITaskOptions {
+  [key: string]: ITask;
+}
+
+class NodeCronTrigger {
+  cronJobs?: {
+    tasks?: ITaskOptions;
+    scheduledTasks?: ScheduledTask[];
+  };
+
+  constructor(tasks: ITaskOptions) {
+    initializeTasks(tasks);
+
+    const scheduledTasks: ScheduledTask[] = [];
+
+    Object.keys(tasks).forEach((task, index) => {
+      // return tasks[task].task()
+      const taskData = tasks[task];
+      scheduledTasks[index] = cron.schedule(taskData.schedule, () => {
+        taskData.task();
+        updateTasksDate(tasks);
+      }, taskData.options || {});
+    });
+
+    this.cronJobs = {
+      tasks,
+      scheduledTasks,
+    };
+  }
+
+  getJobs() {
+    return this.cronJobs;
+  }
+
+  schedule = cron.schedule;
+  validate = cron.validate;
+  getTasks = cron.getTasks;
+}
+
+module.exports = NodeCronTrigger;
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// the value is an object of pairs key / value - key = task name as it's a good behaviour to give your task a name and value = object that have your cron options
+
+import { ITaskOptions } from "./NodeCronTrigger";
+
+// type cronStringSyntax = '* * * * * *';
+
+
+const tasks: ITaskOptions = {
+  "runAt12AM": {
+    schedule: '12 28 8 74 47 5',
+    task() {
+      console.log('hey whatever you did i will run at 12 AM even if after restarting your server');
+    }
+  },
+  "runEach5Mins": {
+    schedule: '',
+    task() {
+      console.log('hey whatever you did i will run each 5 minutes of any hour even if after restarting your server');
+    }
+  },
+};
+
